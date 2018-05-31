b53c82d6b4aa7e54fc9864445afcbdb79913bd58
quine <c@queertra.sh>
2018-05-31 16:36:58 -0700

    added some documentation to xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -4,9 +4,16 @@ import System.Exit (exitWith, ExitCode(..))
 import Data.Map (fromList, insert, delete)
 import qualified XMonad as Xm
 
-myTerminal = "urxvt"
-myModMask  = Xm.mod4Mask
 
+myTerminal :: String
+myTerminal = "urxvt"
+
+myModMask :: Xm.KeyMask
+myModMask  = Xm.mod4Mask -- super/windows key
+
+{-| Edit oldBinds and newBinds to change xmonad's basic keybindings -}
+myKeys :: Xm.XConfig l -> Xm.XConfig Xm.Layout
+            -> Data.Map.Internal.Map (Xm.ButtonMask, Xm.KeySym) (Xm.X ())
 myKeys conf@(Xm.XConfig {Xm.modMask = mod}) =
   insertThese newBinds $ deleteThese oldBinds $ defaultKeys where
     defaultKeys = (Xm.keys def) conf
@@ -14,14 +21,16 @@ myKeys conf@(Xm.XConfig {Xm.modMask = mod}) =
     insertThese kvs m = foldl (\m' (k,v) -> insert k v m') m kvs
     shift = Xm.shiftMask
     -- oldBinds are removed before newBinds are added
-    -- they represent unwanted keybindings
+    -- they represent unwanted keyboard shortcuts
     oldBinds = [ (mod .|. shift, Xm.xK_q)
                , (mod .|. shift, Xm.xK_Return)]
+    -- new ((shortcut), action) keybindings go here
     newBinds = [ ((mod .|. shift, Xm.xK_q), Xm.kill)
                , ((mod .|. shift, Xm.xK_e), Xm.io (exitWith ExitSuccess))
                , ((mod, Xm.xK_Return), Xm.spawn $ Xm.terminal conf)
                ]
 
+main :: IO ()
 main = do
   -- Note: someRecordValue {record = list} is interpreted as
   -- someRecordValue, but with these replacements
@@ -30,3 +39,13 @@ main = do
     , Xm.terminal = myTerminal
     , Xm.keys = myKeys
     }
+
+{-
+Important haskell concepts in use:
+ - The ($) function (function application operator)
+ - Qualified imports (Xm.whatever)
+ - Record datatypes (Those curly-brace, "name = value" lists)
+ - Haskell's very silly-looking preferred indent style for long lists
+ - Pattern matching against Record datatypes, like in myKeys
+ - do syntax and IO
+-}
